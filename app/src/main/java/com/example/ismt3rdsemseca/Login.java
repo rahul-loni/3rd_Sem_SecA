f4a095f21ab40847a57011c1c3f1119b7f4e5345
Rahul Kumar <rahulismt491@gmail.com>
2022-06-09 10:35:25 +0545

    icon in button edit text profile

--- app/src/main/java/com/example/ismt3rdsemseca/Login.java
+++ app/src/main/java/com/example/ismt3rdsemseca/Login.java
@@ -2,13 +2,29 @@ package com.example.ismt3rdsemseca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textView=findViewById(R.id.link);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/rahul-loni"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }
