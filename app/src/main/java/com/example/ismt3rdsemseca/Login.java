ec60596a671fcda99be7dce475172a16d02a28ea
Rahul Kumar <rahulismt491@gmail.com>
2022-06-10 11:45:45 +0545

    create menu

--- app/src/main/java/com/example/ismt3rdsemseca/Login.java
+++ app/src/main/java/com/example/ismt3rdsemseca/Login.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -27,4 +29,10 @@ public class Login extends AppCompatActivity {
         });
 
     }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu, menu);
+//        return true;
+//    }
 }
--- app/src/main/java/com/example/ismt3rdsemseca/Signup.java
+++ app/src/main/java/com/example/ismt3rdsemseca/Signup.java
@@ -3,6 +3,8 @@ package com.example.ismt3rdsemseca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Signup extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 }
